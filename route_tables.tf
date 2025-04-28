2e1bd0c26fd9352cea760dc856ed53af24aeb468
ewan <ewan@T10-LT-1333.centre4.local>
2025-04-28 14:59:04 +0100

    private route table changed to allow outbound internet traffic

--- route_tables.tf
+++ route_tables.tf
@@ -25,6 +25,11 @@ resource "aws_route_table_association" "public_assoc_b" {
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.vpc.id
 
+   route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
   tags = {
     Name = var.private_rt_name
   }
