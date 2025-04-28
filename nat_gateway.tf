00c2f12be9141cf42e22ef54fb3efcf6c787ade0
ewan <ewan@T10-LT-1333.centre4.local>
2025-04-28 13:39:34 +0100

    nat gateway configuration amended

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -7,12 +7,9 @@ resource "aws_nat_gateway" "nat" {
     Name = var.nat_gateway_name
   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
+  depends_on = [aws_internet_gateway.igw]
 }
 
 resource "aws_eip" "nat_eip" {
-  instance = aws_nat_gateway.nat.id
   domain   = "vpc"
 }
